d7c5696f1d98302846575b483c37fd1ecf2df538
tiagoc0sta <tiagodpv@gmail.com>
2024-06-23 00:11:45 -0400

    add cadastrar evento

--- eventoappp/src/main/java/com/eventoappp/controllers/EventoController.java
+++ eventoappp/src/main/java/com/eventoappp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eventoappp.models.Evento;
 import com.eventoappp.repository.EventoRepository;
@@ -25,6 +26,15 @@ public class EventoController {
     	er.save(evento);
         return "redirect/cadastrarEvento";
     }
+    
+    @RequestMapping("/eventos")
+    public ModelAndView listaEventos() {
+    	ModelAndView mv = new ModelAndView("index");
+    	Iterable<Evento> eventos = er.findAll(); 
+    	
+    	mv.addObject("eventos", eventos);
+		return mv;
+     }
 
 
 }
